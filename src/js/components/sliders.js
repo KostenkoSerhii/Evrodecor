0c58e95525105eec4672b29aaa3d95843f2051fa
Serhii <kso_132@mail.ru>
2018-01-13 22:34:52 +0200

    responsive ready

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -5,7 +5,7 @@ function sliders(){
 	viewBox="0 0 199.3 328" enable-background="new 0 0 199.3 328" xml:space="preserve">
 	<polygon points="195.7,36.1 163.4,3.7 3.7,163.7 3.7,163.7 3.7,163.7 163.4,323.7 195.7,291.3 68.4,163.7 "/>
 	</svg></div>`;
-	let heroNextl = `<div class="heroNext"><?xml version="1.0" encoding="utf-8"?>
+	let heroNext = `<div class="heroNext"><?xml version="1.0" encoding="utf-8"?>
 	<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	viewBox="0 0 199.3 328" enable-background="new 0 0 199.3 328" xml:space="preserve">
 	<polygon points="131,163.7 3.7,291.3 36,323.7 195.7,163.7 195.7,163.7 195.7,163.7 36,3.7 3.7,36.1 "/>
@@ -21,8 +21,22 @@ function sliders(){
 	<polygon points="131,163.7 3.7,291.3 36,323.7 195.7,163.7 195.7,163.7 195.7,163.7 36,3.7 3.7,36.1 "/>
 	</svg></div>`;
 
+	let heroSlider = $(".js-hero-slider");
 	let expSlider = $(".js-expir-slider");
-
+	heroSlider.slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: heroPrev,
+		nextArrow: heroNext,
+		dots: true,
+		responsive: [
+		{
+			breakpoint: 992,
+			settings: "unslick"
+		}
+		]
+	});
 	expSlider.slick({
 		infinite: true,
 		slidesToShow: 1,
@@ -30,6 +44,7 @@ function sliders(){
 		prevArrow: expPrev,
 		nextArrow: expNext,
 		dots: true
+
 	});
 
 	//end
