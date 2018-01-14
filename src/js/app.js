68014376c4db9d058b1ad3a8a5c895f0a147be73
Serhii <kso_132@mail.ru>
2018-01-14 02:00:51 +0200

    add send mals

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,23 @@
 import svgUseIt from 'svg-use-it';
 import tabs from './components/tabs.js';
 import sliders from './components/sliders.js';
+import formSubmit from './components/form.js';
+import AOS from 'aos';
 // import scrollMagic from './components/scrollmagic.js';
 // import techAnimation from './components/tech-animation.js';
 import slick from 'slick-carousel';
 
 $(document).ready(function(){
-
 	svgUseIt();
+
 	// before must be set day
 	sliders();
 	tabs();
 	// techAnimation();
-
+	AOS.init({
+		disable: 'mobile'
+	});
+	formSubmit()
 	//end
 });
 
---
+++ src/js/components/form.js
@@ -0,0 +1,19 @@
+function formSubmit(){
+	$('form').submit(function(e){
+		e.preventDefault(); 
+		var form = $(this);
+		var m_method="POST"; 
+		var m_action=$(this).attr('action'); 
+		var m_data=$(this).serialize();
+		$.ajax({
+			type: m_method,
+			url: m_action,
+			data: m_data,
+			success: function(result){ 
+				console.log('success');
+				document.location.href = "page-thanks.html";
+			}
+		}); 
+	});
+};
+module.exports = formSubmit;
